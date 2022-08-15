64f4a26432de0c75b795068ba34c4748e5d83620
wiryadev <abrarwiryawan@gmail.com>
2022-08-15 11:32:29 +0700

    Initial commit

---
+++ app/src/main/java/com/wiryadev/foodapp/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.wiryadev.foodapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Orange500 = Color(0xFFFF815E)
+val Black700 = Color(0xFF212131)
+val White700 = Color(0xFFFAFAFA)
+val Gray400 = Color(0xFFCBCCD5)
+val Gray700 = Color(0xFFAAAAAC)
---
+++ app/src/main/java/com/wiryadev/foodapp/ui/theme/Theme.kt
@@ -0,0 +1,37 @@
+package com.wiryadev.foodapp.ui.theme
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val LightColorPalette = lightColors(
+    primary = Orange500,
+    onPrimary = Color.White,
+    onBackground = Black700,
+    onSurface = Gray400,
+    onSecondary = Gray700,
+
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun FoodAppTheme(
+    content: @Composable () -> Unit,
+) {
+
+    MaterialTheme(
+        colors = LightColorPalette,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
